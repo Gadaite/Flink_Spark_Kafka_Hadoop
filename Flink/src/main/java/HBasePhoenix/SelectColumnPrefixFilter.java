a6ff1379a9ea8b01c8e72016897389b1953d0ebb
Gadaite <LYP809834049>
2022-04-17 00:59:46 +0800

    Gadaite

--- Flink/src/main/java/HBasePhoenix/SelectColumnPrefixFilter.java
+++ Flink/src/main/java/HBasePhoenix/SelectColumnPrefixFilter.java
@@ -3,29 +3,28 @@ package HBasePhoenix;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
-import org.apache.hadoop.hbase.CompareOperator;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.filter.ColumnPrefixFilter;
 import org.apache.hadoop.hbase.util.Bytes;
-
-public class SelectScanColumnHbaseData {
+//  列名前缀过滤器
+public class SelectColumnPrefixFilter {
     public static void main(String[] args) throws Exception{
         Configuration conf = new Configuration();
-        conf.set("hbase.zookeeper.quorum","192.168.1.10");
+        conf.set("hbase.zookeeper.quorum","192.168.1.10:2181");
         Connection conn = ConnectionFactory.createConnection(conf);
         Table gadaite = conn.getTable(TableName.valueOf("Gadaite"));
-        Scan columnscan = new Scan();
-        //  这里如果为没有满足筛选条件的值的时候会将空值所在的数据进行返回
-        SingleColumnValueFilter columnfilter = new SingleColumnValueFilter(Bytes.toBytes("F1"), Bytes.toBytes("F12"),
-                CompareOperator.EQUAL,Bytes.toBytes("G1.G12"));
-        columnscan.setFilter(columnfilter);
-        ResultScanner scanner = gadaite.getScanner(columnscan);
+        Scan scan = new Scan();
+        //  值返回指定列簇的数据
+        ColumnPrefixFilter f2 = new ColumnPrefixFilter(Bytes.toBytes("F2"));
+        scan.setFilter(f2);
+        ResultScanner scanner = gadaite.getScanner(scan);
         for (Result sc:scanner){
             for (Cell c:sc.rawCells()){
                 System.out.println(c);
                 System.out.println(new String(CellUtil.cloneValue(c)));
             }
         }
+
     }
 }
--- Flink/src/main/java/HBasePhoenix/SelectSetFilter.java
+++ Flink/src/main/java/HBasePhoenix/SelectSetFilter.java
@@ -6,26 +6,34 @@ import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.CompareOperator;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.filter.ColumnPrefixFilter;
+import org.apache.hadoop.hbase.filter.FilterList;
+import org.apache.hadoop.hbase.filter.RegexStringComparator;
+import org.apache.hadoop.hbase.filter.RowFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 
-public class SelectScanColumnHbaseData {
+//  多条件过滤集合
+
+public class SelectSetFilter {
     public static void main(String[] args) throws Exception{
         Configuration conf = new Configuration();
         conf.set("hbase.zookeeper.quorum","192.168.1.10");
         Connection conn = ConnectionFactory.createConnection(conf);
         Table gadaite = conn.getTable(TableName.valueOf("Gadaite"));
-        Scan columnscan = new Scan();
-        //  这里如果为没有满足筛选条件的值的时候会将空值所在的数据进行返回
-        SingleColumnValueFilter columnfilter = new SingleColumnValueFilter(Bytes.toBytes("F1"), Bytes.toBytes("F12"),
-                CompareOperator.EQUAL,Bytes.toBytes("G1.G12"));
-        columnscan.setFilter(columnfilter);
-        ResultScanner scanner = gadaite.getScanner(columnscan);
+        FilterList list = new FilterList();
+        Scan scan = new Scan();
+        RowFilter rowFilter = new RowFilter(CompareOperator.EQUAL, new RegexStringComparator("002"));
+        ColumnPrefixFilter f2 = new ColumnPrefixFilter(Bytes.toBytes("F2"));
+        list.addFilter(rowFilter);
+        list.addFilter(f2);
+        scan.setFilter(list);
+        ResultScanner scanner = gadaite.getScanner(scan);
         for (Result sc:scanner){
             for (Cell c:sc.rawCells()){
                 System.out.println(c);
                 System.out.println(new String(CellUtil.cloneValue(c)));
             }
         }
+
     }
 }
