4c31adfcb7ac8740a50ad6b48ee63dc0700180c6
Gadaite <LYP809834049>
2022-04-27 02:11:07 +0800

    Gadaite

--- Spark/src/main/scala/RemootEnv/UselocalWebUI.scala
+++ Spark/src/main/scala/RemootEnv/UselocalWebUI.scala
@@ -7,11 +7,11 @@ object UselocalWebUI {
   def main(args: Array[String]): Unit = {
     //程序在运行时receiver会独占一个线程,所以streaming程序至少要两个线程,防止starvation scenario
     val spark = SparkSession.builder().appName("StreamingWordCount")
-      .config("spark.cores.max","2").config("spark.master","spark://192.168.1.10:7077").master("local[*]").getOrCreate()
-//      .config("spark.cores.max","2").master("local[*]").getOrCreate()
+      .config("spark.driver.host","192.168.1.10")
+      .config("spark.wui.showConsoleProgress","false").master("yarn").getOrCreate()
     val sc = spark.sparkContext
     sc.setLogLevel("WARN")
-    val conf = sc.getConf
+//    val conf = sc.getConf
     //所有流功能的主要入口
     val ssc: StreamingContext = new StreamingContext(sc , Seconds(5))
     val stream: ReceiverInputDStream[String] =  ssc.socketTextStream("192.168.1.10",7777)
