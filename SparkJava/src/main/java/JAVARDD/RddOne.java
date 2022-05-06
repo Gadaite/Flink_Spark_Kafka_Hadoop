eaae9345d2a8649f31984b326b61b46fe6d10d98
Gadaite <LYP809834049>
2022-05-06 19:38:19 +0800

    Gadaite

---
+++ SparkJava/src/main/java/JAVARDD/RddOne.java
@@ -0,0 +1,21 @@
+package JAVARDD;
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
+public class RddOne {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder().config("spark.ui.showConsoleProgress", "false")
+                .appName("RddOne").master("local[*]").enableHiveSupport().getOrCreate();
+        SparkContext sc = spark.sparkContext();
+        sc.setLogLevel("ERROR");
+        Dataset<Long> data = spark.range(1, 10);
+        spark.sql("use hive_test_one");
+        spark.sql("select * from audi limit 10").show();
+        spark.sql("show databases").show();
+        data.show();
+
+//        JavaSpark
+    }
+}
