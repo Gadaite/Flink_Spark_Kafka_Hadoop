965a73c2648c448d0b0099e7caca41874d06d27d
Gadaite <LYP809834049>
2022-05-07 20:29:11 +0800

    Gadaite

---
+++ SparkJava/src/main/java/JavaDataFrame/DataFrameFromHive.java
@@ -0,0 +1,18 @@
+package JavaDataFrame;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class DataFrameFromHive {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder().config("spark.ui.showConsoleProgress", "false").appName("DataFrameFromHive")
+                .master("local[*]").enableHiveSupport().getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
+        spark.sql("show databases").show();
+        spark.sql("use hive_test_one");
+        Dataset<Row> sqldf = spark.sql("select * from audi limit 10");
+        sqldf.show();
+        sqldf.printSchema();
+    }
+}
