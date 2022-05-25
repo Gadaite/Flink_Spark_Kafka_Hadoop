e6963c89249a00dd43d694974a90ee488f8b134b
Gadaite <LYP809834049>
2022-05-25 19:13:01 +0800

    Gadaite

--- SparkJava/src/main/java/CETC10S/ToolStartRunJob/ExecSparkJob.java
+++ SparkJava/src/main/java/CETC10S/ToolStartRunJob/ExecSparkJob.java
@@ -1,13 +1,16 @@
 package CETC10S.ToolStartRunJob;
 
+import CETC10S.ToolBaseSparkApp.BaseSparkENV;
 import CETC10S.ToolConnectDB.MysqlJDBC;
+import org.apache.spark.status.api.v1.BaseAppResource;
 
-public class ExecSparkJob extends MysqlJDBC {
+public class ExecSparkJob extends MysqlJDBC implements BaseSparkENV {
     public static void main(String[] args) throws Exception{
         MysqlJDBC jdbc = new MysqlJDBC();
         String driver = jdbc.init().getProperty("driver");
         String url = jdbc.init().getProperty("url");
         String username = jdbc.init().getProperty("username");
         String password = jdbc.init().getProperty("pwd");
+
     }
 }
