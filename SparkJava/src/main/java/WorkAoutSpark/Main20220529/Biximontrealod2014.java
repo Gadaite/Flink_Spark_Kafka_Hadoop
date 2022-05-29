d10e4d996c3582781627ff84044a2eeee45ff83f
Gadaite <LYP809834049>
2022-05-29 15:25:24 +0800

    Gadaite

---
+++ SparkJava/src/main/java/WorkAoutSpark/Main20220529/Biximontrealod2014.java
@@ -0,0 +1,89 @@
+package WorkAoutSpark.Main20220529;
+
+/**
+ * table name:  BIXIMontrealOD2014
+ * author name: Gadaite
+ * create time: 2022-05-29 15:19:58
+ */ 
+public class Biximontrealod2014 extends EntityHelper{
+
+	private java.lang.Integer Column1;
+	private java.lang.String start_date;
+	private java.lang.Integer start_station_code;
+	private java.lang.String end_date;
+	private java.lang.Integer end_station_code;
+	private java.lang.Integer duration_sec;
+	private java.lang.Integer is_member;
+
+	public Biximontrealod2014() {
+		super();
+	}
+	public Biximontrealod2014(java.lang.Integer Column1,java.lang.String start_date,java.lang.Integer start_station_code,java.lang.String end_date,java.lang.Integer end_station_code,java.lang.Integer duration_sec,java.lang.Integer is_member) {
+		this.Column1=Column1;
+		this.start_date=start_date;
+		this.start_station_code=start_station_code;
+		this.end_date=end_date;
+		this.end_station_code=end_station_code;
+		this.duration_sec=duration_sec;
+		this.is_member=is_member;
+	}
+	public void setColumn1(java.lang.Integer Column1){
+		this.Column1=Column1;
+	}
+	public java.lang.Integer getColumn1(){
+		return Column1;
+	}
+	public void setStart_date(java.lang.String start_date){
+		this.start_date=start_date;
+	}
+	public java.lang.String getStart_date(){
+		return start_date;
+	}
+	public void setStart_station_code(java.lang.Integer start_station_code){
+		this.start_station_code=start_station_code;
+	}
+	public java.lang.Integer getStart_station_code(){
+		return start_station_code;
+	}
+	public void setEnd_date(java.lang.String end_date){
+		this.end_date=end_date;
+	}
+	public java.lang.String getEnd_date(){
+		return end_date;
+	}
+	public void setEnd_station_code(java.lang.Integer end_station_code){
+		this.end_station_code=end_station_code;
+	}
+	public java.lang.Integer getEnd_station_code(){
+		return end_station_code;
+	}
+	public void setDuration_sec(java.lang.Integer duration_sec){
+		this.duration_sec=duration_sec;
+	}
+	public java.lang.Integer getDuration_sec(){
+		return duration_sec;
+	}
+	public void setIs_member(java.lang.Integer is_member){
+		this.is_member=is_member;
+	}
+	public java.lang.Integer getIs_member(){
+		return is_member;
+	}
+	@Override
+	public String toString() {
+		return "BIXIMontrealOD2014[" + 
+			"Column1=" + Column1 + 
+			", start_date=" + start_date + 
+			", start_station_code=" + start_station_code + 
+			", end_date=" + end_date + 
+			", end_station_code=" + end_station_code + 
+			", duration_sec=" + duration_sec + 
+			", is_member=" + is_member + 
+			"]";
+	}
+	@Override
+	public String getPrimaryKey() {
+		return "null";
+	}
+}
+
--- SparkJava/src/main/java/WorkAoutSpark/Main20220529/HandleTable.java
+++ SparkJava/src/main/java/WorkAoutSpark/Main20220529/HandleTable.java
@@ -9,13 +9,10 @@ import GadaiteToolConnectDB.AutoCreateMysqlBean;
 import GadaiteToolConnectDB.MysqlConnect;
 import GadaiteToolConnectDB.MysqlJdbcCon;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-import java.io.File;
-import java.util.ArrayList;
 
 public class HandleTable extends MysqlConnect {
     public static void main(String[] args) throws Exception{
@@ -27,27 +24,30 @@ public class HandleTable extends MysqlConnect {
         AutoCreateMysqlBean createMysqlBean = new AutoCreateMysqlBean();
         AutoCreateMysqlBean.basePath = FilePath;
         AutoCreateMysqlBean.packageOutPath = PackageName;
-        File file = new File("F:\\CodeG50\\BiGData\\SparkJava\\src\\main\\java\\WorkAoutSpark\\Main20220529\\Audi.java");
-        if (!file.getParentFile().exists()) {
-            createMysqlBean.generateTables = new String[]{"audi"};
-            createMysqlBean.generate();
-        }else {
-            System.out.println("Audi.Java is alreadly exists !");
-        }
+        createMysqlBean.generateTables = new String[]{"BIXIMontrealOD2014"};
+        createMysqlBean.generate();
+
         /**
          * 调用封装类，使用创建Spark执行环境，并获取数据源
          */
         MysqlJdbcCon con = new MysqlJdbcCon();
         SparkSession spark = con.getSparkSesssion("handleTable", "ERROR");
-        Dataset<Row> dataset = con.GetDataSetByProperties(spark, "select * from audi limit 5");
-        dataset.show();
-        dataset.printSchema();
+        Dataset<Row> datasetOD2014 = con.GetDataSetByProperties(spark, "select * from BIXIMontrealOD2014");
+        datasetOD2014.show();
+        datasetOD2014.printSchema();
+
+        Dataset<Row> datasetStations2014 = con.GetDataSetByProperties(spark, "select * from BIXIMontrealStations2014");
+        datasetStations2014.show();
+        datasetStations2014.printSchema();
         /**
          * 测试生成的JavaBean文件的可用性
          */
-        JavaRDD<Row> audiRDD = dataset.toJavaRDD();
-        JavaRDD<Audi> audimodelRDD = audiRDD.map(new MMapFunction());
-        audimodelRDD.foreach(x -> System.out.println(x));
+        JavaRDD<Row> OD2014RDD = datasetOD2014.toJavaRDD();
+//        JavaRDD<Audi> audimodelRDD = BIXIMontrealOD2014RDD.map(new MMapFunction());
+//        audimodelRDD.foreach(x -> System.out.println(x));
+        /**
+         *
+         */
 
     }
 }
