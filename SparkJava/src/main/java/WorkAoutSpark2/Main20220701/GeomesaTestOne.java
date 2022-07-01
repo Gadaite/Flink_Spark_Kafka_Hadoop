3f3d0ffbad9970a177eccb97227cd9c541fe04dc
Gadaite <LYP809834049>
2022-07-01 23:42:05 +0800

    Gadaite

--- SparkJava/src/main/java/WorkAoutSpark2/Main20220701/GeomesaTestOne.java
+++ SparkJava/src/main/java/WorkAoutSpark2/Main20220701/GeomesaTestOne.java
@@ -2,13 +2,11 @@ package WorkAoutSpark2.Main20220701;
 
 import GadaiteToolConnectDB.PostgresqlJdbcCon;
 import org.apache.spark.sql.SparkSession;
-import org.locationtech.geomesa.spark.GeoMesaSparkKryoRegistrator;
 
 public class GeomesaTestOne {
     public static void main(String[] args) throws Exception{
         PostgresqlJdbcCon pcon = new PostgresqlJdbcCon();
         SparkSession spark = pcon.getSparkSesssion("Geomesa", "ERROR");
-        spark.conf().set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-        spark.conf().set("spark.kryo.registrator",GeomesaSpa );
+        spark.sql("show databases").show();
     }
 }
