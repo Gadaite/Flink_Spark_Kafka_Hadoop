8e182c85c04db0136b42e24613abd40e45d67c49
Gadaite <LYP809834049>
2022-07-06 20:36:53 +0800

    Gadaite

--- Spring/src/main/java/AOP/InterfaceImpl1.java
+++ Spring/src/main/java/AOP/InterfaceImpl1.java
@@ -4,7 +4,6 @@ public class InterfaceImpl1  implements Interface1{
     public Integer add(Integer num1, Integer num2) {
         return num1+num2;
     }
-
     public String update(String id) {
         return id;
     }
--- Spring/src/main/java/AOP/JDKProxy.java
+++ Spring/src/main/java/AOP/JDKProxy.java
@@ -10,15 +10,17 @@ public class JDKProxy {
     public static void main(String[] args) {
 
         //  创建接口实现类的代理对象
-        Class[] inter = {InterfaceImpl1.class};
+        Class[] inter = {Interface1.class};
         InterfaceImpl1 interfaceImpl1 = new InterfaceImpl1();
-        InterfaceImpl1 res =
-                (InterfaceImpl1) Proxy.newProxyInstance(JDKProxy.class.getClassLoader(), inter, new proxy(interfaceImpl1));
+        Object o = Proxy.newProxyInstance(JDKProxy.class.getClassLoader(), inter, new proxy(interfaceImpl1));
+        /**
+         * JDK动态代理是代理的接口，因此强制转换应该转换为接口，而不是实现类，若强制转换实现类就会抛出ClassCastException，
+         * 好比ArrayList与LinkedList实现统一接口List，两者也不能相互转换，但都可以向上转型
+         */
+        Interface1 res = (Interface1) o;
         System.out.println(res.add(111, 222));
         System.out.println(res.update(UUID.randomUUID().toString()));
-
     }
-
     public static class proxy implements InvocationHandler{
         //  传递被代理的对象
         private Object object;
